ab0fcce7ea5bb4a16dd13a2f6442132ef25b512b
ta17eee <ta17eee@gmail.com>
2025-05-31 15:56:19 +0900

    feat: カレンダーの外枠だけ作成

--- WillDo/Views/CalendarView.swift
+++ WillDo/Views/CalendarView.swift
@@ -5,14 +5,126 @@
 //  Created by 高野　泰生 on 2025/05/31.
 //
 
+import Foundation
 import SwiftUI
 
 struct CalendarView: View {
+    @State var monthOffset: Int = 0
+    private let week: [String] = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Spacer()
+            VStack(alignment: .center, spacing: 8) {
+                Spacer()
+                HStack(spacing: 32) {
+                    Button("<") {
+                        monthOffset -= 1
+                    }
+                    Text("\(getTargetYear())")
+                    Text("/")
+                    Text("\(getTargetMonth())")
+                    Button(">") {
+                        monthOffset += 1
+                    }
+                }
+                .font(.title)
+                HStack(spacing: 8) {
+                    ForEach(week, id: \.self) { day in
+                        ZStack {
+                            Color.clear
+                            Text("\(day)")
+                                .foregroundColor(getDayColor(for: day))
+                        }
+                        .frame(height: 32)
+                    }
+                }
+                ForEach(getMonthCalendar(offset: monthOffset), id: \.self) { week in
+                    HStack(spacing: 8) {
+                        ForEach(week, id: \.self) { day in
+                            if day == 0 {
+                                Color.clear
+                            }
+                            else {
+                                Button(action: {
+                                    
+                                }) {
+                                    ZStack {
+                                        RoundedRectangle(cornerRadius: 4)
+                                            .fill(Color.white)
+                                            .shadow(radius: 1)
+                                        VStack {
+                                            Text("\(day)")
+                                                .foregroundStyle(Calendar.getDayColor(day: day, monthOffset: monthOffset))
+                                                .padding(4)
+                                            Spacer()
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Spacer()
+        }
+    }
+    
+    private func getTargetMonth() -> String{
+        let now = Date()
+        let target = Calendar.current.date(byAdding: .month, value: monthOffset, to: now)!
+        return String(Calendar.current.component(.month, from: target))
+    }
+    
+    private func getTargetYear() -> String {
+        let now = Date()
+        let target = Calendar.current.date(byAdding: .month, value: monthOffset, to: now)!
+        return String(Calendar.current.component(.year, from: target))
+    }
+    
+    private func getDayColor(for day: String) -> Color {
+        switch day {
+        case "sun":
+            return .red
+        case "sat":
+            return .cyan
+        default:
+            return .primary
+        }
+    }
+    
+    private func getMonthCalendar(offset: Int) -> [[Int]] {
+        let calendar = Calendar.current
+        let now = Date()
+        let target = calendar.date(byAdding: .month, value: offset, to: now)!
+        
+        let daysCount = calendar.daysInMonth(for: target)
+        let firstDayOfMonth = calendar.date(from: calendar.dateComponents([.year, .month], from: target))!
+        let firstDay = calendar.component(.weekday, from: firstDayOfMonth)
+        
+        var result: [[Int]] = []
+        var day = 1
+        
+        for weekRow in 0..<6 {
+            var week: [Int] = []
+            
+            for weekdayColumn in 1...7 {
+                let isEmptyDay = (weekRow == 0 && weekdayColumn < firstDay) || day > daysCount
+                week.append(isEmptyDay ? 0 : day)
+                if !isEmptyDay {
+                    day += 1
+                }
+            }
+
+            result.append(week)
+        }
+        
+        return result
     }
 }
 
 #Preview {
-    CalendarView()
+    TabView {
+        CalendarView()
+    }
 }
